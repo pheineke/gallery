45b36b3af76ba7990e2c7e7bc5f764cd9951ff4b
pheineke <heineke.pe@gmail.com>
2024-09-22 20:23:00 +0200

    requirements for Docker

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,7 @@
+### Flask Webserver Executable
+### For Deployment
+###
+
 import os
 from flask import Flask, render_template, send_from_directory
 
@@ -6,4 +10,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
